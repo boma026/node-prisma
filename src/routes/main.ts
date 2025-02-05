dcf1249f6812d164169837dc98fb04d7a4874de2
biome-ti <boma026>
2025-02-05 11:29:44 -0300

    05/02/2025

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,19 @@
 import { Router } from 'express';
+import { prisma } from '../libs/prisma';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.get("/user", async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            name: "Boma",
+            email: "arthurboma@yahoo.com.br"
+        }
+    });
+
+    res.json({ user });
+})
